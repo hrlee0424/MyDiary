271943df4a89d134c171833ba5c5f859caa6e5ee
hrlee0424 <ehdqkd770@naver.com>
2021-05-05 17:14:51 +0900

    WriteActivity

--- app/src/main/java/polarbear/mydiary/WriteActivity.kt
+++ app/src/main/java/polarbear/mydiary/WriteActivity.kt
@@ -4,15 +4,20 @@ import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.room.Room
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
 import java.util.*
 
 class WriteActivity : AppCompatActivity() {
+    val TAG = "WriteActivity"
     var db : AppDatabase? = null
 
     @SuppressLint("ClickableViewAccessibility")
@@ -22,6 +27,9 @@ class WriteActivity : AppCompatActivity() {
 
         val editDate : EditText = findViewById(R.id.editDate)
         val btnSave : Button = findViewById(R.id.btnSave)
+        val editTitle : EditText = findViewById(R.id.editTitle)
+        val editContents : EditText = findViewById(R.id.editContents)
+        val editTodo : EditText = findViewById(R.id.editTodo)
 
         db = AppDatabase.getInstance(this)
 
@@ -45,15 +53,33 @@ class WriteActivity : AppCompatActivity() {
             true
         }
 
+
         btnSave.setOnClickListener {
             /*val db = Room.databaseBuilder(
                 applicationContext,
                 AppDatabase::class.java, "WriteDiary"
             ).build()*/
-            val content = WriteEntity("2020-05-04", "오늘하루는?", "어땠나요?", "할일", "202005041526")
-            db?.diaryDao()?.insertAll(content)
+            val mDate = editDate.text.toString()
+            val mTitle = editTitle.text.toString()
+            val mContents = editContents.text.toString()
+            val mTodo = editTodo.text.toString()
+//            val dateTime : LocalDateTime = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//                LocalDateTime.now()
+            //현재날짜시간
+            val now : Long = System.currentTimeMillis()
+            val date = Date(now)
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale("ko", "KR"))
+            val stringTime = dateFormat.format(date)
+//            val longTime = dateFormat.parse(stringTime).time
+            Log.i(TAG, "onCreate stringTime:$stringTime")
 
-            finish()
+            if(mDate.isEmpty() || mTitle.isEmpty() || mContents.isEmpty() || mTodo.isEmpty()){
+                Toast.makeText(applicationContext, "양식에 맞게 내용을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }else{
+                val content = WriteEntity(mDate, mTitle, mContents, mTodo, stringTime)
+                db?.diaryDao()?.insertAll(content)
+                finish()
+            }
         }
     }
 }
